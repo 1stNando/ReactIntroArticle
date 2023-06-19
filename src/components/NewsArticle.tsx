18776012f78cc78aed8b4406aad031ccbbf9c83f
Fernando Aguilar <sparkmanperspective@gmail.com>
2023-06-19 19:40:33 -0400

    Created a function component for NewsArticle(). So we can reuse this later in the code.

---
+++ src/components/NewsArticle.tsx
@@ -0,0 +1,8 @@
+// Simply tells the editor AND 'vite' that this file deals with React and JSX
+import React from 'react'
+
+// Export -- is so we can use this outside of this file
+// Function. PasCalCased. Returns JSX.
+export function NewsArticle() {
+  return <div>Something</div>
+}
