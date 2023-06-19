f9a0858d6c93d10a82cae9d3fed697ba296fbb80
Fernando Aguilar <sparkmanperspective@gmail.com>
2023-06-19 19:02:44 -0400

    Initial commit. npm installed

--- src/main.tsx
+++ src/main.tsx
@@ -9,3 +9,5 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 )
+// We generally will not be touching any code in this file.
+// Our main ts/js React code will be located in the App.tsx
