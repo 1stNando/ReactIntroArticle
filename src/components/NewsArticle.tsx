b99ecfabe1bb5e2e68363b0daa815bc47e99a366
Fernando Aguilar <sparkmanperspective@gmail.com>
2023-06-19 20:18:41 -0400

    Explicitly defined a NewsArticleProps

--- src/components/NewsArticle.tsx
+++ src/components/NewsArticle.tsx
@@ -1,9 +1,15 @@
 // Simply tells the editor AND 'vite' that this file deals with React and JSX
 import React from 'react'
 
+// Step 2 to fix red lines under props found in NewsArticle. This helps check for errors. Every property, whats its name and type explicitly.
+type NewsArticleProps = {
+  title: string
+  body: string
+}
+
 // Export -- is so we can use this outside of this file
 // Function. PasCalCased. Returns JSX.
-export function NewsArticle(props) {
+export function NewsArticle(props: NewsArticleProps) {
   return (
     <article className="intro-article">
       <h2 className="article-title">{props.title}</h2>
